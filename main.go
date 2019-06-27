4a277c0c5670faa6c2ddf484bfb3fcd3ee0a4e49
Divyanshu Singh <dsinecos@gmail.com>
2019-06-27 18:06:12 +0530

    add example composing different types of generators

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 func main() {
 	fmt.Println("Hello World!")
 
-	const MAX = 10
+	const MAX = 30
 	isDivisibleByThree := g.IsDivisibleBy(3)
+	isDivisibleByFive := g.IsDivisibleBy(5)
 
-	fmt.Println("Printing odd integers")
-	for num := range isDivisibleByThree(g.Integer(10)) {
+	fmt.Println("Printing integers divisible by 5 and 3")
+	for num := range isDivisibleByFive(isDivisibleByThree(g.Integer(MAX))) {
 		fmt.Println(num)
 	}
 }
