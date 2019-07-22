4feb9f7ffefc5b0ca1523703d886c6b93517fe14
Divyanshu Singh <dsinecos@gmail.com>
2019-07-22 18:17:38 +0530

    add test cases for merge, split, splitRnd and pipeline

---
+++ chanutil/chanutil_test.go
@@ -0,0 +1,82 @@
+package chanutil
+
+import "testing"
+
+const checkMark = "\u2713"
+const ballotX = "\u2717"
+
+func TestMerge(t *testing.T) {
+	t.Log("Testing Merge")
+	{
+		t.Log("\t Merges input from multiple channels into a single channel")
+		{
+		}
+
+		t.Log("\t Closes output channel once all the input channels are closed")
+		{
+		}
+
+		t.Log("\t Allows to close input and output channels using a shutdown channel")
+		{
+		}
+
+		t.Log("\t Does not allow to write on the output channel\n")
+		{
+		}
+	}
+}
+
+func TestSplit(t *testing.T) {
+	t.Log("Testing Split")
+	{
+		t.Log("\t Broadcasts (duplicates) input from a single channel across multiple output channels")
+		{
+		}
+
+		t.Log("\t Closes output channels once input channel has been closed and all values have been read from the output channels")
+		{
+		}
+
+		t.Log("\t Allows to close input and output channels using a shutdown channel\n")
+		{
+		}
+	}
+}
+
+func TestSplitRnd(t *testing.T) {
+	t.Log("Testing SplitRnd")
+	{
+		t.Log("\t Distributes input from a single channel across multiple output channels randomly")
+		{
+		}
+
+		t.Log("\t Closes output channels once input channel has been closed and values have been read from the output channels")
+		{
+		}
+
+		t.Log("\t Allows to close input and output channels using a shutdown channel\n")
+		{
+		}
+	}
+}
+
+func TestPipeline(t *testing.T) {
+	t.Log("Testing Pipeline")
+	{
+		t.Log("\t Create an asynchronous pipeline which filters values from the input channel in order of the functions provided and publishes qualifying values to the output channel")
+		{
+		}
+
+		t.Log("\t Shuts down the pipeline and the output channel once the input channel is closed and all the values have been read from the output channel")
+		{
+		}
+
+		t.Log("\t Allows to close the input channel, the pipeline and output channel using a shutdown channel")
+		{
+		}
+
+		t.Log("\t Does not allow to write on the output channel\n")
+		{
+		}
+	}
+}
